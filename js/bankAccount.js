e21408572f5624c782738dc8cd6356db3f9dfad9
ElahehDaneshfar <elahehdaneshfar@gmail.com>
2022-05-04 07:41:29 -0500

    bank acc

---
+++ js/bankAccount.js
@@ -0,0 +1,30 @@
+const button = document.getElementById("createButton");
+
+function clickHandler(event) {
+    console.log('Button Clicked');
+    var nametxt = document.getElementById("name").value;
+    var deposittxt = document.getElementById("deposit").value;
+    var textArea = document.getElementById("showarea");
+
+    var account;
+    account = new BankAccount();
+    BankAccount.accountInfoList.push(account.createAccount(nametxt, deposittxt));
+
+    textArea.value += account.getTextArea;
+}
+
+class BankAccount {
+    static accountInfoList = [];
+    #name;
+    #balance;
+    createAccount(name, deposit) {
+        this.#name = name;
+        this.#balance = parseFloat(deposit);
+    }
+
+    get getTextArea(){
+        return "Account Name : " + this.#name + "  Balance : " + this.#balance + "\n ";
+    }
+}
+
+button.addEventListener("click", clickHandler);
